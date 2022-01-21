7d24dd1e4a5ff6c2ff591ea3acfff04c8514dd60
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-21 20:24:34 +0300

    wip

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://ralph_ralph:r1a2l3ph@cluster0.kmch0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
